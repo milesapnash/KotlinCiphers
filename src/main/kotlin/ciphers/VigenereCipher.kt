793bba37c877abbdd71e40bf2416444c5f251a18
Miles Nash <mapnash@gmail.com>
2024-01-28 16:09:54 +0000

    Add Vigenère Cipher

---
+++ src/main/kotlin/ciphers/VigenereCipher.kt
@@ -0,0 +1,34 @@
+package ciphers
+
+fun vigenereEncrypt(plainText: String, key: String): String {
+    var keyIndex = 0
+    return plainText.map {
+        val keyChar = key[keyIndex]
+        val baseChar = if (keyChar.isUpperCase()) 'A' else 'a'
+        keyIndex = (keyIndex + 1) % key.length
+
+        charShift(it, keyChar - baseChar)
+    }.joinToString("")
+}
+
+fun vigenereDecrypt(cipherText: String, key: String): String {
+    var keyIndex = 0
+    return cipherText.map {
+        val keyChar = key[keyIndex]
+        val baseChar = if (keyChar.isUpperCase()) 'A' else 'a'
+        keyIndex = (keyIndex + 1) % key.length
+
+        charShift(it, -(keyChar - baseChar))
+    }.joinToString("")
+}
+
+private fun charShift(inputChar: Char, shift: Int): Char {
+    if (!inputChar.isLetter()) {
+        return inputChar
+    }
+
+    val baseChar = if (inputChar.isUpperCase()) 'A' else 'a'
+    val encryptedCharCode = ((inputChar - baseChar + shift) + 26) % 26 + baseChar.code
+
+    return encryptedCharCode.toChar()
+}
